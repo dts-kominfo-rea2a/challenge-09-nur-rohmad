11ceb6be7f10a5bf8977418f12778e28fbe564ad
nur-rohmad <nurrohmad776@gmail.com>
2022-06-19 13:36:00 +0700

    pengumpulan tugas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Contact from "./components/Contact";
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header />
+      {contacts.map((data) => {
+        return (
+          <Contact
+            foto={data.photo}
+            nama={data.name}
+            noHP={data.phone}
+            email={data.email}
+          />
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
+import "./contact.css";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = (props) => {
+  return (
+    <div className="card">
+      <div className="kanan">
+        <img className="foto" src={props.foto} alt="profil" />
+      </div>
+      <div className="kiri">
+        <p className="nama">{props.nama}</p>
+        <p className="noHp">{props.noHP}</p>
+        <p className="email">{props.email}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
